93edbe15fb8d89faaf0e40590f67469c0cb622f3
Oleksandra <sashakirina@gmail.com>
2024-05-25 14:18:12 +0200

    2 task

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,3 +1,17 @@
-export const TodoList = () => {
-  return <h3>TodoList</h3>;
+import { Grid, TodoListItem } from '..';
+
+export const TodoList = ({ todos, deleteItem }) => {
+  return (
+    <Grid>
+      {todos.map(({ id, text }, index) => (
+        <TodoListItem
+          key={id}
+          text={text}
+          count={index + 1}
+          deleteItem={deleteItem}
+          id={id}
+        />
+      ))}
+    </Grid>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,33 @@
-import { Text } from 'components';
+import { Form, Text, TodoList } from 'components';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState([]);
+  function addTodo(text) {
+    const todo = {
+      text,
+      id: nanoid(),
+    };
+    setTodos(prevTodo => {
+      return [...prevTodo, todo];
+    });
+  }
+
+  const deleteItem = id => {
+    setTodos(prevTodo => {
+      return prevTodo.filter(todo => todo.id !== id);
+    });
+  };
+
+  console.log(todos);
+  return (
+    <>
+      <Form onSubmit={addTodo} />
+      {todos.length === 0 && (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
+      <TodoList todos={todos} deleteItem={deleteItem} />
+    </>
+  );
 };
