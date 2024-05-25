64043a4f497746d1c93c838c0918b2cdd2e5edf1
Iryna P <irpolg@ukr.net>
2024-05-25 16:31:40 +0300

    2blended-1task

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,15 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { Grid } from "../Grid/Grid";
+import { PhotosGalleryItem } from "../PhotosGalleryItem/PhotosGalleryItem";
+
+export const PhotosGallery = ({ photos }) => {
+  return <Grid>
+  {
+     photos.map((photo) => 
+       <PhotosGalleryItem
+         key={photo.id}
+         avg_color={photo.avg_color}
+         src={photo.src}
+         alt={photo.alt} />)
+  }
+</Grid>;
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,40 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Text, Form, PhotosGallery, Button } from 'components';
+import { useState, useEffect } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+
+  const handleSearch = (text) => {
+    setQuery(text);
+  };
+
+  const addPage = () => {
+    setPage(page + 1);
+  }
+
+  useEffect(() => {
+    if (!query) return
+
+    const getData = async () => {
+      try {
+        const data = await getPhotos(query, page);
+        setPhotos(prevPhotos => [...prevPhotos, ...data.photos]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getData()
+  }, [query, page])
+
   return (
     <>
-      <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={ handleSearch} />
+      {photos.length === 0 && <Text textAlign="center">Let`s begin search 🔎</Text>}
+      <PhotosGallery photos={photos} />
+      <Button onClick={addPage}>Load more...</Button>
     </>
   );
 };
